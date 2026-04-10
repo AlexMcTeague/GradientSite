fdfb4df37e590a4f15559f05c3287b837250ecf1
AlexMcTeague <mcteaguealex@gmail.com>
2026-04-09 22:54:03 -0400

    Added events for color changing (pending functionality)

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const lastColor = document.getElementById('lastColor');
 const message = document.getElementById('inputMessage');
 const result = document.getElementById('result');
 
+// Set the color of each character in the string to a gradient between the first and last colors
+const refreshGradient = () => {
+    
+}
+
 // Set the result text equal to the input message, or the placeholder if the input is empty
 const refreshText = () => {
     result.textContent = (message.value == '') ? message.placeholder : message.value;
+    refreshGradient();
 }
 
 // Initialize the result text and gradient on page load
@@ -13,4 +19,12 @@ refreshText();
 
 message.addEventListener('input', () => {
     refreshText();
+});
+
+firstColor.addEventListener('change', () => {
+    refreshGradient();
+});
+
+lastColor.addEventListener('change', () => {
+    refreshGradient();
 });
