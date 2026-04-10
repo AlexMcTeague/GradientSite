269baa54e3922c311ed8dfc27ff4111202837294
AlexMcTeague <mcteaguealex@gmail.com>
2026-04-09 22:53:27 -0400

    Moved text updates to a function for re-use

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const lastColor = document.getElementById('lastColor');
 const message = document.getElementById('inputMessage');
 const result = document.getElementById('result');
 
-result.textContent = (message.value == '') ? message.placeholder : message.value;
+// Set the result text equal to the input message, or the placeholder if the input is empty
+const refreshText = () => {
+    result.textContent = (message.value == '') ? message.placeholder : message.value;
+}
+
+// Initialize the result text and gradient on page load
+refreshText();
 
 message.addEventListener('input', () => {
-    result.textContent = message.value;
+    refreshText();
 });
